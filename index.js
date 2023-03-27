ce9fce3813e552db3c08697a6d9196d8f1fca889
prudence <prudence.jerono@student.moringaschool.com>
2023-03-27 22:07:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function counting() {
+  let countDown = 10;
+  while (countDown ) {
+    10();
+    9();
+    8();
+    7();
+    6();
+    5();
+    4();
+    3();
+    2();
+    1();
+    0();
+    countDown = countDown < 10;
+  } 
+}
